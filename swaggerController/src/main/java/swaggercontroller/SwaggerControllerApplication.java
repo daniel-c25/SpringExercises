8228703be5f201749e0670daa5c4a4d405e8921f
Daniel <adi251007@gmail.com>
2024-04-30 20:41:09 +0200

    added swagger exercises

---
+++ swaggerController/src/main/java/swaggercontroller/SwaggerControllerApplication.java
@@ -0,0 +1,13 @@
+package swaggercontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerControllerApplication.class, args);
+	}
+
+}
